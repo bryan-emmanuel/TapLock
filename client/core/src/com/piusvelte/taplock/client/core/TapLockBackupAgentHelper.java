22f8767478151ea59fdc52ec23682f1b54526232
piusvelte <piusvelte@gmail.com>
2012-12-21 12:39:22 -0500

    update backup agent

---
+++ client/core/src/com/piusvelte/taplock/client/core/TapLockBackupAgentHelper.java
@@ -0,0 +1,37 @@
+/*
+ * Tap Lock
+ * Copyright (C) 2012 Bryan Emmanuel
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  Bryan Emmanuel piusvelte@gmail.com
+ */
+package com.piusvelte.taplock.client.core;
+
+import android.annotation.TargetApi;
+import android.app.backup.BackupAgentHelper;
+import android.app.backup.SharedPreferencesBackupHelper;
+import android.util.Log;
+
+@TargetApi(8)
+public class TapLockBackupAgentHelper extends BackupAgentHelper {
+	private static final String TAG = "TapLockBackupAgentHelper";
+
+	@Override
+	public void onCreate() {
+		Log.d(TAG, "onCreate");
+		SharedPreferencesBackupHelper spbh = new SharedPreferencesBackupHelper(this, TapLock.KEY_PREFS);
+		addHelper(TapLock.KEY_PREFS, spbh);
+	}
+}
